58a1b4ada1480805ebba9291c31b4b4d431dc2ac
MrVauxs <mrvauxs@gmail.com>
2023-12-27 03:43:59 +0100

    Build

--- _dist/zod/bundles/bySource.js
+++ _dist/zod/bundles/bySource.js
@@ -4,7 +4,9 @@ import { source } from "../source.js";
 import { content } from "../content.js";
 import { nonEmpty } from "../utils/nonEmpty.js";
 export const bySource = z
-	.record(datatypes, source.or(z.array(content)))
+	.object({ source: source })
+	// TODO: Replace z.string() with proper datatype keys.
+	.and(z.record(z.string(), z.array(content)))
 	.describe(
 		"A bundle of all data in a given source. The `source` key is required and maps to the `source` object; all other keys map from a datatype present in the source to an array of objects of that datatype.",
 	)
