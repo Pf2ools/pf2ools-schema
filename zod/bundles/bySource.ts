736a1ee709145492f74f93001e16e78076ffb173
MrVauxs <mrvauxs@gmail.com>
2023-12-28 06:36:41 +0100

    Updoot bySource

--- zod/bundles/bySource.ts
+++ zod/bundles/bySource.ts
@@ -3,11 +3,12 @@ import { z } from "zod";
 import { datatype } from "../indexes/datatypes.js";
 import { data } from "../_data.js";
 import { nonEmpty } from "../utils/nonEmpty.js";
+import { source } from "../source.js";
 
 export const bySource = z
-	.record(datatype, z.array(data).min(1))
+	.object({ source })
+	.and(z.record(datatype, z.array(data).min(1)))
 	.describe(
 		"A bundle of all content in a given source. Keys map from a datatype to an array of objects of that datatype. The `source` key is required.",
 	)
-	.refine(...nonEmpty)
-	.refine((obj) => obj.source);
+	.refine(...nonEmpty);
