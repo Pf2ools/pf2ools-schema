9a0c1e9a1af4c809e15c70ab9d2524f171279a65
MrVauxs <mrvauxs@gmail.com>
2024-02-11 09:28:35 +0100

    Build

--- _dist/zod/bundles/bySource.js
+++ _dist/zod/bundles/bySource.js
@@ -1,14 +1,15 @@
-import { z } from "zod";
+import { ZodArray, z } from "zod";
 import { dataTypesArray } from "../indexes/datatypes.js";
 import * as data from "../_index.js";
 import { nonEmpty } from "../utils/nonEmpty.js";
 import { source } from "../source.js";
-const bySourceObject = { source: z.array(source) };
-const dataTypesArrayOmitSource = dataTypesArray.filter((datatype) => datatype !== "source");
-dataTypesArrayOmitSource.forEach((datatype) => {
-    bySourceObject[datatype] = z.array(z.array(data[datatype]).min(1)).optional();
-});
+const bySourceObj = new Map(dataTypesArray.map((datatype) => {
+    return [
+        datatype,
+        datatype === "source" ? z.array(data[datatype]).min(1) : z.array(data[datatype]).min(1).optional(),
+    ];
+}));
 export const bySource = z
-    .object(bySourceObject)
+    .object(Object.fromEntries(bySourceObj))
     .describe("A bundle of all content in a given source. Keys map from a datatype to an array of objects of that datatype. The `source` key is required.")
     .refine(...nonEmpty);
--- zod/bundles/bySource.ts
+++ zod/bundles/bySource.ts
@@ -5,9 +5,14 @@ import * as data from "../_index.js";
 import { nonEmpty } from "../utils/nonEmpty.js";
 import { source } from "../source.js";
 
-const bySourceObj = new Map(dataTypesArray.map((datatype) => {
-	return [datatype, datatype === "source" ? z.array(data[datatype]).min(1) : z.array(data[datatype]).min(1).optional()];
-}));
+const bySourceObj = new Map(
+	dataTypesArray.map((datatype) => {
+		return [
+			datatype,
+			datatype === "source" ? z.array(data[datatype]).min(1) : z.array(data[datatype]).min(1).optional(),
+		];
+	}),
+);
 
 export const bySource = z
 	.object(Object.fromEntries(bySourceObj))
