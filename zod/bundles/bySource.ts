01d0029e7c65c0080f9b83dde696228ab2fa0a31
MrVauxs <mrvauxs@gmail.com>
2024-02-11 15:13:26 +0100

    In my stroke of genius I modified the wrong file

--- zod/bundles/bySource.ts
+++ zod/bundles/bySource.ts
@@ -1,13 +1,10 @@
 import { z } from "zod";
 
-import { datatypeSansSource } from "../indexes/datatypes.js";
-import { data } from "../_data.js";
 import { nonEmpty } from "../utils/nonEmpty.js";
-import { source } from "../source.js";
+import { schemaIndex } from '../_data.js';
 
 export const bySource = z
-	.object({ source: z.array(source) })
-	.and(z.record(datatypeSansSource, z.array(data).min(1)))
+.object({ ...schemaIndex, source: schemaIndex.source.unwrap() })
 	.describe(
 		"A bundle of all content in a given source. Keys map from a datatype to an array of objects of that datatype. The `source` key is required.",
 	)
