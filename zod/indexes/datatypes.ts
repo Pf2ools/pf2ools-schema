45fb60769ce21708b62f5e3ba37fcc711e743d97
MrVauxs <mrvauxs@gmail.com>
2024-02-11 09:13:46 +0100

    This should work as intended.

--- zod/indexes/datatypes.ts
+++ zod/indexes/datatypes.ts
@@ -1,29 +1,29 @@
 import { z } from "zod";
 
 // TODO: find a way to auto-generate this?
+export const contentDatatypesArray = [
+	"background",
+	"condition",
+	"divineIntercession",
+	"domain",
+	"event",
+	"familiarAbility",
+	"relicGift",
+	"skill",
+] as const;
+
+export const metaDatatypesArray = ["license", "source", "sourceGroup"] as const;
+
+export const dataTypesArray = [...contentDatatypesArray, ...metaDatatypesArray] as const;
+
 export const contentDatatype = z
-	.enum([
-		"background",
-		"condition",
-		"divineIntercession",
-		"domain",
-		"event",
-		"familiarAbility",
-		"relicGift",
-		"skill",
-	])
+	.enum(contentDatatypesArray)
 	.describe("A content datatype's name, as it appears in the JSON data itself.");
 
-
-export const metaDatatype = z.enum(["license", "source", "sourceGroup"])
+export const metaDatatype = z.enum(metaDatatypesArray)
 	.describe("A metafile datatype's name, as it appears in the JSON data itself.");
 
-const metaDatatypeSansSource = metaDatatype.exclude(["source"]);
-
-export const datatypeSansSource = contentDatatype.or(metaDatatypeSansSource);
-
-export const datatype = contentDatatype
-	.or(metaDatatype)
+export const datatype = z.enum(dataTypesArray)
 	.describe("A datatype's name, as it appears in the JSON data itself.");
 
 export const datatypes = z.array(datatype).describe("A list of datatypes used by pf2ools-data.").min(1);
