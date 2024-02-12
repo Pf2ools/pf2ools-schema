8738fed98602b1a24d49f107b70f51d75a46bc36
Spappz <34202141+Spappz@users.noreply.github.com>
2024-02-12 01:16:52 +0000

    Update datatypes.ts
    no longer needed

--- zod/indexes/datatypes.ts
+++ zod/indexes/datatypes.ts
@@ -18,10 +18,6 @@ export const metaDatatype = z
 	.enum(["license", "source", "sourceGroup"])
 	.describe("A metafile datatype's name, as it appears in the JSON data itself.");
 
-const metaDatatypeSansSource = metaDatatype.exclude(["source"]);
-
-export const datatypeSansSource = contentDatatype.or(metaDatatypeSansSource);
-
 export const datatype = contentDatatype
 	.or(metaDatatype)
 	.describe("A datatype's name, as it appears in the JSON data itself.");
