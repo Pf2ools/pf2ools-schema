50f3be38bd846342f237b7a6f8e396da2a5e860d
Spappz <34202141+Spappz@users.noreply.github.com>
2024-04-27 00:19:39 +0100

    no more ajv
    the world has moved past the need for ajv

--- scripts/test-data.js
+++ scripts/test-data.js
@@ -11,46 +11,24 @@ import { getJSONsRecursively } from "./utils/getFilesRecursively.js";
 const program = new Command()
 	.summary("validate data for Pf2ools")
 	.description(
-		`Validates a file or directory of files against the Pf2ools schema. Only JSON files will be tested. Tests can be performed using:\n\t- JSON Schema via Ajv ${chalk.dim(
-			"(--test ajv)",
-		)}\n\t- Zod ${chalk.dim(
-			"(--test zod)",
-		)}\nAll methods are roughly equivalent, but some may have more or less specific validation for certain edge cases.`,
+		"Validates a file or directory of files against the Pf2ools schema. Only JSON files will be tested.",
 	)
 	.argument("<paths...>", "File or directory paths to test")
-	.option("-a, --all", `Test all files ${chalk.dim("(default: break at first validation failure)")}`)
+	.option("-a, --all", `Test all files ${chalk.dim("(default: break at first validationResult failure)")}`)
 	.option("-b, --bundle", "Test data-bundle files rather than data files")
-	.option("-e, --error", "Print only the validation status of failing files")
+	.option("-e, --error", "Print only the validationResult status of failing files")
 	.option("-r, --recurse", "Recursively test files in directories")
 	.option(
 		"-s, --summary",
-		`Suppress printing of validation status for all files and only summarise results ${chalk.dim(
+		`Suppress printing of validationResult status for all files and only summarise results ${chalk.dim(
 			"(note: implies --all)",
 		)}`,
 	)
-	.option("-t, --test <methods...>", `Define validation methods ${chalk.dim("(default: Zod only)")}`)
 	.parse(process.argv);
 
 // Load and validate arguments
 const opts = program.opts();
 if (opts.summary) opts.all = true;
-const testMethods = ["ajv", "zod"];
-if (opts.test) {
-	opts.test.forEach((method) => {
-		if (!testMethods.includes(method))
-			program.error(
-				chalk.red(
-					`"${method}" is not a valid test method\n${chalk.dim(
-						`Available methods: "${testMethods.join('", "')}"`,
-					)}`,
-				),
-				{
-					exitCode: 1,
-					code: "invalid.testMethod",
-				},
-			);
-	});
-} else opts.test = ["zod"];
 let files = [];
 for (const arg of program.args) {
 	const argClean = path.join(...arg.toString().split(localPathSeparator));
@@ -88,19 +66,14 @@ if (!files.length) {
 	process.exit();
 }
 
-// Get validation functions
-import * as ajv from "./test-data-ajv.js";
-import * as zod from "./test-data-zod.js";
-const methods = {
-	ajv: {
-		data: ajv.validateData,
-		bundle: ajv.validateBundle,
-	},
-	zod: {
-		data: zod.validateData,
-		bundle: zod.validateBundle,
-	},
-};
+// Set validationResult function
+import { data } from "../_dist/zod/_data.js";
+import { bundle } from "../_dist/zod/_bundle.js";
+function validate(schema, test) {
+	const result = schema.safeParse(test);
+	if (result.success) return { success: true };
+	return { success: false, error: result.error.issues };
+}
 
 // Validate files
 const failed = `\t${chalk.red("[Failed]")}  `;
@@ -122,15 +95,13 @@ for (const file of files) {
 			});
 		}
 	}
-	const validationFailure = opts.test
-		.map((method) => methods[method][opts.bundle ? "bundle" : "data"](testJSON))
-		.find((result) => !result.success);
-	if (validationFailure) {
-		console.log(failed + file);
-		if (!opts.all) throw validationFailure.error;
-		failCount++;
-	} else {
+	const validationResult = validate(opts.bundle ? bundle : data, testJSON);
+	if (validationResult.success) {
 		if (!opts.error && !opts.summary) console.log(chalk.dim(passed + file));
+	} else {
+		console.log(failed + file);
+		if (!opts.all) throw validationResult.error;
+		failCount++;
 	}
 }
 
@@ -138,18 +109,18 @@ for (const file of files) {
 if (opts.summary || files.length > 1) {
 	if (failCount === 0) {
 		console.log(
-			chalk.green(`All ${chalk.bold(files.length)} file${files.length !== 1 ? "s" : ""} passed validation.`),
+			chalk.green(`All ${chalk.bold(files.length)} file${files.length !== 1 ? "s" : ""} passed validationResult.`),
 		);
 	} else {
 		program.error(
 			chalk.red(
 				`${chalk.bold(failCount)} file${failCount !== 1 ? "s" : ""} (${
 					Math.round((1000 * failCount) / files.length) / 10
-				}%) failed validation.`,
+				}%) failed validationResult.`,
 			),
 			{
 				exitCode: 1,
-				code: "validation.failure",
+				code: "validationResult.failure",
 			},
 		);
 	}
